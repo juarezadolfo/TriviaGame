e6a04410f9b28e700b7f103af13f13fc14786146
juarezadolfo <juarezadolfo@hotmail.com>
2017-12-04 23:21:58 -0800

    final commit after styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,11 +9,39 @@ var triviaQuestions = [{
     answerList: ["Warner Brothers' Coyote and Roadrunner Cartoons", "Keyboard Synthesizer", "Soundcloud.com", "Foley Artist"],
     answer: 0
 }, {
-    question: "How many different Challengers were used in the filming of the movie Vanishing Point??",
+    question: "How many different Challengers were used in the filming of the movie 'Vanishing Point?'",
     answerList: ["6", "10", "7", "5"],
     answer: 3
+}, {
+    question: "What popular 80's TV show featured a high flying MOPAR called 'The General Lee'?",
+    answerList: ["The A-Team", "Knight Rider", "Miami Vice", "The Dukes of Hazzard"],
+    answer: 3
+}, {
+    question: "What is the most popular MOPAR paint color?",
+    answerList: ["Candy Apple Red", "Big Bad Orange", "Plum Crazy", "SL Silver"],
+    answer: 2
+}, {
+    question: "What is currently the fastest production car in the world?",
+    answerList: ["Bugati Veyron", "Tesla Model S", "Audi A8", "Dodge Challenger Demon"],
+    answer: 3
+}, {
+    question: "What does 'HEMI' stand for?",
+    answerList: ["The Shape of a Mopar's Cylinder Heads", "The Shape of the Steering Wheel", "A MOPAR Paint Color", "The Brake Caliper Size"],
+    answer: 0
+}, {
+    question: "What is a 440 six-pack?",
+    answerList: ["The price of beer back in 1970", "The size of a HEMI engine and its carburetor", "John and Horace Dodge's favorite cocktail", "None of the Above"],
+    answer: 1
+}, {
+    question: "In which year was the convertible top first offered as an option for the Plymouth Roadrunner?",
+    answerList: ["1968", "1969", "1970", "1971"],
+    answer: 1
+}, {
+    question: "What car was featured in epic the car chase scenes2007 Quentin Taratino film 'Death Proof'?",
+    answerList: ["1969 Dodge Charger", "1970 Dodge Challenger", "1970 Chevy Nova", "All of the Above"],
+    answer: 3
 }];
-var jpgArray = ['question1', 'question2', 'question3'];
+var jpgArray = ['question1', 'question2', 'question3','question4', 'question5', 'question6','question7', 'question8', 'question9', 'question10'];
 var currentQuestion;
 var correctAnswer;
 var incorrectAnswer;
@@ -77,7 +105,7 @@ function newQuestion() {
         answerPage();
     });
 }
- //function for timer going down
+//function for timer going down
 function countdown() {
     seconds = 15;
     $('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
@@ -97,7 +125,7 @@ function showCountdown() {
 //Clear questions page
 function answerPage() {
     $('#currentQuestion').empty();
-    $('.thisChoice').empty(); 
+    $('.thisChoice').empty();
     $('.question').empty();
 
     var rightAnswerText = triviaQuestions[currentQuestion].answerList[triviaQuestions[currentQuestion].answer];
